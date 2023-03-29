4f41ae67d1afd76aed77e2e8ca81815dc4af5dd3
Enzo Pain <enzo.pain.etu@univ-lille.fr>
2023-03-29 20:34:40 +0200

    add dummy encoder

--- server/src/main/java/eu/euromov/activmotiv/config/ActivMotivConfig.java
+++ server/src/main/java/eu/euromov/activmotiv/config/ActivMotivConfig.java
@@ -13,12 +13,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import eu.euromov.activmotiv.authentication.DummyPasswordEncoder;
 import eu.euromov.activmotiv.authentication.ParticipantService;
 import jakarta.persistence.EntityManagerFactory;
 
@@ -46,16 +44,12 @@ public class ActivMotivConfig {
 	public UserDetailsService userDetailsService() {
 		return new ParticipantService();
 	}
-
-	@Bean
-	public static PasswordEncoder passwordEncoder() {
-	    return NoOpPasswordEncoder.getInstance();
-	}
 	
 	@Bean
 	public AuthenticationProvider authenticationProvider(UserDetailsService service) {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(service);
+		provider.setPasswordEncoder(new DummyPasswordEncoder());
 		return provider;
 	}
 	
